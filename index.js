ba8c337a357b6a3bbb3dd6088dc3009c160dd004
Jake Rosoman <jkroso@gmail.com>
2014-01-21 23:42:48 +1300

    pull Test class of the constructor

--- index.js
+++ index.js
@@ -8,13 +8,7 @@
 module.exports = function(hydro, _) {
   var focus = false;
   var enabled = false;
-  var RootTest = null;
-
-  if (typeof Hydro === 'undefined') {
-    Root = require('hydro').Test;
-  } else {
-    RootTest = Hydro.Test;
-  }
+  var RootTest = hydro.constructor.Test;
 
   RootTest.SyncTest.prototype.focus =
   RootTest.AsyncTest.prototype.focus = function() {
